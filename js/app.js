cd5c960b429f5197ed52b8c5b97ee34ec42fc802
Justin Morris <justinjodymorris@gmail.com>
2018-03-12 15:40:17 -0700

    created literal objects for each location and a randCust function in each object

--- js/app.js
+++ js/app.js
@@ -1,1 +1,70 @@
 'use strict';
+
+// object {
+//   max customer: 
+//   min customers
+//   avg cookies / customer
+//   customers per hour = rand num
+//   cookies purchased per hour = avg cookies purchased * rand number of customers generated
+// }
+
+
+var firstAndPike = {
+  minCust: 23,
+  maxCust: 65,
+  avgCookiesPerSale: 6.3,
+  custPerHr: function() {
+    return Math.ceil(Math.random() * ((this.maxCust) - (this.minCust)) + this.minCust);
+  },
+  cookiesPerHour: function() {
+    return this.avgCookiesPerSale * this.custPerHr();
+  },
+}
+
+var seaTac = {
+  minCust: 3,
+  maxCust: 24, 
+  avgCookiesPerSale: 1.2,
+  custPerHr: function() {
+    return Math.ceil(Math.random() * ((this.maxCust) - (this.minCust)) + this.minCust);
+  },
+  cookiesPerHour: function() {
+    return this.avgCookiesPerSale * this.custPerHr();
+  }
+}
+
+var seattleCenter = {
+  minCust: 11,
+  maxCust: 38,
+  avgCookiesPerSale: 3.7,
+  custPerHr: function() {
+    return Math.ceil(Math.random() * ((this.maxCust) -(this.minCust)) + this.minCust); 
+  },
+  cookiesPerHr: function() {
+    return this.avgCookiesPerSale * this.custPerHr();
+  }
+}
+
+var capitolHill = {
+  minCust: 20,
+  maxCust: 38,
+  avgCookiesPerSale: 2.3,
+  custPerHr: function () {
+    return Math.ceil(Math.random() * ((this.maxCust) - (this.minCust)) + this.minCust);
+  },
+  cookiesPerHour: function () {
+    return this.avgCookiesPerSale * this.custPerHr();
+  }
+}
+
+var alki = {
+  minCust: 2,
+  maxCust: 16,
+  avgCookiesPerSale: 4.6,
+  custPerHr: function () {
+    return Math.ceil(Math.random() * ((this.maxCust) - (this.minCust)) + this.minCust);
+  },
+  cookiesPerHour: function () {
+    return this.avgCookiesPerSale * this.custPerHr();
+  }
+}
